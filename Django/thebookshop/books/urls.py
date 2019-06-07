42dd4c6f540c2add98bec44c68b342b21be6d811
Dvoryaninov_Alex <a.dvoryan@gmail.com>
2019-06-07 23:30:40 +0300

    Auth+

--- Django/thebookshop/books/urls.py
+++ Django/thebookshop/books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from books.views import *
 
 urlpatterns = [
-    path('list', BookListView.as_view(), name='book-list-view'),    
+    path('all', BookListView.as_view(), name='book-list-view'),    
     path('<int:pk>', BookDetailView.as_view(), name='book-detail-view'),
     path('create', BookCreateView.as_view(), name='book-create-view'),
     path('<int:pk>/update', BookUpdateView.as_view(), name='book-update-view'),
